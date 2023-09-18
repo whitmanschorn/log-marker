c839a5e6ed7e86591c1b467a53be7f917004ddb6
Whitman Schorn <whitman.schorn@gmail.com>
2023-09-18 05:14:52 -0500

    working v1

--- main.ts
+++ main.ts
@@ -1,134 +1,88 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+  MarkdownView,
+  CachedMetadata,
+  Notice,
+  Plugin,
+  TFile,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface DailyEntrySettings {
+  mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: DailyEntrySettings = {
+  mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class DailyEntryPlugin extends Plugin {
+  settings: DailyEntrySettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => this.createAndPush());
+  }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+  onunload() {}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+  // the first function that runs
+  async createAndPush() {
+    // const app = window.app as App;
+    const todaysDate = new Date().toDateString()
+    const currentTime = new Date().toLocaleTimeString();
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    let newContent = currentTime + '\n'
+    let inlineSetting = ""
+    let newTitle = todaysDate 
+    let shouldPrepend = false;
 
-	onunload() {
+    // check if syntax to push file is present
+    try {
+      // check if file exists already
 
-	}
+      var fileWithName: TFile | undefined;
+      var baseTitleName = newTitle;
+      if (baseTitleName.includes("/")) {
+        var pathParts = baseTitleName.split("/");
+        baseTitleName = pathParts[pathParts.length - 1];
+      }
+      const files = this.app.vault.getMarkdownFiles().filter(item => {
+        return item.basename == baseTitleName
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+      });
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+      fileWithName = undefined;
+      // append content or create new file
+      if(files.length === 0){
+        new Notice(`Creating file and appending timestamp...`);
+        const newFile = await this.app.vault.create(newTitle + ".md", newContent);
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+      } else {
+        fileWithName = files[0];
+        new Notice(`File already exists. Appending content...`);
+        const fullExistingFileText = await this.app.vault.read(fileWithName);
+        var existingFileText = ""
+        await this.app.vault.modify(fileWithName, fullExistingFileText + "\n\n" + newContent)
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+      }
+    await this.app.workspace.openLinkText(newTitle, "", false);
+      const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+    const editor = view.editor;
+        editor.setCursor(editor.lastLine())
+        editor.focus()
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+      } catch (err) {
+        new Notice(`Unhandled error. Doing nothing`)
+      }
+    }
+  
 }
